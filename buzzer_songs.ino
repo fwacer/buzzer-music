91099d77b61e0778c0dab01ba91776b2179f624e
Bryce <30963872+fwacer@users.noreply.github.com>
2017-09-05 13:15:10 -0700

    Update buzzer_songs.ino

--- buzzer_songs.ino
+++ buzzer_songs.ino
@@ -1,29 +1,25 @@
-#define SONG_LENGTH 30
+#define MAX_SONG_LENGTH 30
 int pin = 9;
 
-/*
-int mary_lamb[][SONG_LENGTH]   = {{3,2,1,2,3,3,3, 2,2,2, 3,5,5, 3,2,1,2,3,3,3, 2,1,2,3,2,1, -1},
-                                  {0,0,0,0,1,1,5, 1,1,5, 1,1,7, 0,0,0,0,1,1,5, 1,1,1,1,1,7, -1}
-                                 };
-*/
 //first level of array = note
 //second level         = note length (4 = 1/2, 2 = 1/8, 1 = 1/16)
 //third level          = delay after note
-char mary_lamb[][SONG_LENGTH] = {{'e','d','c','d','e','e','e', 'd','d','d', 'e','g','g', 'e','d','c','d','e','e','e', 'd','c','d','e','d','c', -1},
+
+char mary_lamb[][MAX_SONG_LENGTH] = {{'e','d','c','d','e','e','e', 'd','d','d', 'e','g','g', 'e','d','c','d','e','e','e', 'd','c','d','e','d','c', -1},
                                   {2,2,2,2,2,2,2, 2,2,2, 2,2,2, 2,2,2,2,2,2,2, 2,2,2,2,2,2, -1},
                                   {0,0,0,0,1,1,5, 1,1,5, 1,1,7, 0,0,0,0,1,1,5, 1,1,1,1,1,7, -1}
                                  };
-char amazing_grace_v1[][SONG_LENGTH] = {{'d','g', 'b','a','g', 'b', 'a', 'g', 'e','d', 'd','g', 'b','a','g','b', 'a','d', -1},
-                                                  {2,4, 1,2,2, 4, 4, 4, 2,4, 2,4, 1,1,2, 4,2, 6, -1},
+char amazing_grace_p1[][MAX_SONG_LENGTH] = {{'d','g', 'b','a','g', 'b', 'a', 'g', 'e','d', 'd','g', 'b','a','g','b', 'a','h'/*600*/, -1},
+                                                  {2,4, 1,1,1, 3, 3, 4, 2,4, 2,4, 1,1,2, 4,2, 6, -1},
                                                   {2,2, 0,0,2, 3, 2, 3, 2,3, 2,3, 0,0,2, 3,2, 6, -1}
                                                  };
-char amazing_grace_v2[][SONG_LENGTH] = {{'d','d', 'b','b', 'a','g', 'e','d', 'd','g', 'g','b', 'a','g', -1},
+char amazing_grace_p2[][MAX_SONG_LENGTH] = {{'h','h', 'b','b', 'a','g', 'e','d', 'd','g', 'g','b', 'a','g', -1},
                                                   {2,4, 2,4, 2,4, 2,4, 2,4, 2,4, 2,4, -1},
                                                   {2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, -1}
-                                                 };
-void readSong(char notes[][SONG_LENGTH]){
+                                                 };                                                 
+void readSong(char notes[][MAX_SONG_LENGTH]){
   int i = 0;
-  for(i=0; i< SONG_LENGTH; i++){
+  for(i=0; i< MAX_SONG_LENGTH; i++){
     if (notes[0][i]==-1){
       return;
     }
@@ -48,7 +44,9 @@ void play(char note, int duration, int len){
     break;
     case 'g':  freq = 392;
     break;
-    default: freq = 600;
+    case 'h':  freq = 600; //has the right frequency, not sure what note it should be.
+    break;
+    default: freq = 800;
     Serial.println("default triggered");
     break;
   }
@@ -63,13 +61,14 @@ void play(char note, int duration, int len){
 }
 void setup() {
   Serial.begin(9600);
-  readSong(mary_lamb);
-  delay(3000);
-  readSong(amazing_grace_v1);
-  delay(2000);
-  readSong(amazing_grace_v2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readSong(mary_lamb);
+  delay(3000);
+  
+  readSong(amazing_grace_p1);
+  delay(500);
+  readSong(amazing_grace_p2);
+  delay(3000);
 }
